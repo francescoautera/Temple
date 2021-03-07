b472fd5da5f6bdd0a371f82e0ed98aa5b5b8f462
Federico Rispoli <federicorispoli96@gmail.com>
2021-03-07 10:27:23 +0100

    Add raycast

---
+++ Temples/Assets/Lezione-15/RayCast.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace L15.Fruit
+{
+    public class RayCast : MonoBehaviour
+    {
+        [SerializeField] public GameObject projectile;
+        [SerializeField] public float range;
+        GameObject target;
+        public Transform spawnPoint;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            range = 100f;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+
+            if (Input.GetMouseButton(0))
+            {
+                Debug.Log("Bang");
+
+                Ray pos = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+
+                int layerMask = 1 << 2;
+
+                layerMask = ~layerMask;
+
+                if (Physics.Raycast(pos,out hit, range, layerMask));
+                {               
+                    Destroy(hit.collider.gameObject);
+                }
+            }
+        }
+    }
+}
